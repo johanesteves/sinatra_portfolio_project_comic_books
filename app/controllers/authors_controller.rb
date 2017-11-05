f559c1e950889a6a5507b11b742b5911d0beab98
johanesteves <jsesteves91@gmail.com>
2017-11-04 21:29:32 -0400

    Added logic to create issues and comicbooks via authors/new

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,12 +11,15 @@ class AuthorsController < ApplicationController
   end
 
   post '/authors' do
-    new_author = Author.new(params[:author_name])
+    new_author = Author.new(name: params[:author_name])
 
-    if params[:issue].empty? && param[:comicbook_title].empty?
-      "empty"
-    else
-      "full"
+    if !params[:issue].values.all? {|i| i.empty?} && !params[:comicbook_title].empty?
+      new_issue = Issue.create(title: params[:issue][:title], issue_number: params[:issue][:issue_number].to_i, cover_date: params[:issue][:cover_date].to_i)
+
+      comicbook = Comicbook.find_or_create_by(title: params[:comicbook_title]) << new_issue
+      comicbook.save
+
+      new_author.comicbooks << comicbook
     end
 
     binding.pry
