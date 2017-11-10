c6aeb7aad9deb4425774ebc430d8b5d9f3c88851
johanesteves <jsesteves91@gmail.com>
2017-11-10 14:29:59 -0500

    Added signup validation to allow only one username in DB

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,16 +30,22 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    user = User.new(params[:user])
-
-    if user.save
-      session[:user_id] = user.id
-      flash[:success] = "You have sucessfully signed-up"
-      redirect '/comicbooks'
+    binding
+    if User.find_by_username(params[:user][:username])
+      flash[:danger] = "This username already exists!"
+      redirect '/signup'
     else
-      flash[:danger] = "Signup unsucessful. Please try again"
-      redirect "/login"
+      user = User.new(params[:user])
+      if user.save
+        session[:user_id] = user.id
+        flash[:success] = "You have sucessfully signed-up"
+        redirect '/comicbooks'
+      else
+        flash[:danger] = "Signup unsucessful. Please try again"
+        redirect "/login"
+      end
     end
+
   end
 
   get '/logout' do
